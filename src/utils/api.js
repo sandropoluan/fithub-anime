118de279638fed4e125681b52860c9f63c525290
Sandro <sandro.brayen@shopee.com>
2025-02-27 19:50:33 +0700

    API request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,17 @@
-export function getList(filterParams){
-
-    
+export async function getAnimeList(filterParams) {
+    const keys = Object.keys(filterParams);
+    let params = '';
+    if (keys.length) {
+        params += '?';
+        for (let key of keys) {
+            params += `${key}=${filterParams[key]}&`;
+        }
+    }
+    try {
+        const res = await fetch(`https://api.jikan.moe/v4/anime+${params}`);
+        const json = await res.json();
+        return json;
+    } catch (e) {
+        throw (e)
+    }
 }
