865ce3f53fa6fda6b960cbb1b57adc41e4d6b8f0
Sandro <sandro.brayen@shopee.com>
2025-03-01 14:15:27 +0700

    Use hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
 import LandingPage from './components/LandingPage';
 import Detail from './components/Detail';
 
 export default function RoutesComponent() {
-   return <BrowserRouter basename="/fithub-anime">
-        <Routes>
-            <Route index element={<LandingPage/>} />
-            <Route path='detail/:id' element={<Detail/>} />
-        </Routes>
-    </BrowserRouter>
+    return <HashRouter>
+            <Routes>
+                <Route index element={<LandingPage />} />
+                <Route path='/detail/:id' element={<Detail />} />
+            </Routes>
+    </HashRouter>
 }
